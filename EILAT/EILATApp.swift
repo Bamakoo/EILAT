1194f5254ee60ef002ab181bf0f00ef65c3d21a3
Emma GAUBERT <emma.gaubert@decathlon.com>
2024-01-14 16:29:52 +0100

    Initial Commit

---
+++ EILAT/EILATApp.swift
@@ -0,0 +1,17 @@
+//
+//  EILATApp.swift
+//  EILAT
+//
+//  Created by Emma GAUBERT on 14/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct EILATApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
